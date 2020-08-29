e1de842c923f5758498024c8d7502b6ace02d2e0
Ljuniorcode <lucianojuniorarl@gmail.com>
2020-08-29 12:31:04 -0300

    07 - ajustando o header

--- assets/js/navbar-animation-fix.js
+++ assets/js/navbar-animation-fix.js
@@ -1,8 +1,8 @@
 $('#collapse-navbar').on('show.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
+	$('.banner').css('transform', 'translate(-50%, 10%');
 
 });
 
 $('#collapse-navbar').on('hide.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+	$('.banner').css('transform', 'translate(-50%, -50%');
 });
